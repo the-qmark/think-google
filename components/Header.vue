41b7250bb06332c57a4aac73cbaaa4380548020e
the-qmark <egormalyukow@gmail.com>
2022-02-14 22:58:24 +0300

    init

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="header">HEADER</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+$text-color: rgb(140, 0, 255);
+
+.header {
+  color: $text-color;
+}
+</style>
