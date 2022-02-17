3abca732eb6e517f51bbd168da6710535d5b93ee
the-qmark <egormalyukow@gmail.com>
2022-02-17 14:04:38 +0300

    Добавил две кнопки

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
-    <div class="footer">MY FOOTER</div>
+    <div class="footer">
+      <div class="footer__wrapper">BUTTON 1</div>
+      <div class="footer__wrapper">BUTTON 2</div>
+    </div>
   </div>
 </template>
 
@@ -16,5 +19,15 @@ export default {
   margin-top: 50px;
   height: 100px;
   display: flex;
+  justify-content: space-between;
+}
+
+.footer__wrapper {
+  background-color: rgb(114, 36, 124);
+  color: #fff;
+  width: 150px;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 100px;
 }
 </style>
